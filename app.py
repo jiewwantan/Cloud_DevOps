68c137ba2fb43d1e8e98f303dd19c652c4b27041
JWT <jiewwan@gmail.com>
2022-02-28 12:42:35 +0800

    initial commit

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ def hello_world():
     
     return 'Hello, World!'
 
+@app.route('/btc')
+def hello_eslam():
+    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    data = response.json()
+    btcValue = "<h1> BTC price "+data["bpi"]["USD"]["rate"]+" USD </h1>"
+    return btcValue
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
