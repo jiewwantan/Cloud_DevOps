4db0c748f6e8c705688e6772bf96c6a8f95e864b
JWT <jiewwan@gmail.com>
2022-03-01 05:56:36 +0800

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+xx
 @app.route('/')
 def hello_world():
     
-    return 'Hello, 2022'
+    return 'Hello, World!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
